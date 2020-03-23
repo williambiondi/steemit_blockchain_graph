892ecb36bf258d604fbc44baa8eb77f30f0ac712
William <williambiondi997@gmail.com>
2020-03-23 16:31:38 +0100

    fix

--- graph_builder.py
+++ graph_builder.py
@@ -27,7 +27,7 @@ def read_rewards(graph):
                 try:
                     #graph.nodes[reward['account']][1]['rewards_steem'] += int(reward['reward_steem']['amount'])
                     #graph.nodes[reward['account']][1]['rewards_sbd'] += int(reward['reward_sbd']['amount'])
-                    graph.nodes[reward['account']][1]['rewards_vests'] = int(reward['reward_vests']['amount'])
+                    graph.nodes[reward['account']]['rewards_vests'] += int(reward['reward_vests']['amount'])
                     #graph.nodes[reward['account']][1]['last_reward'] = op['timestamp']
                     #print("New claimed reward for"+reward['acccount']+"  on: "+op['timestamp'])
                 except KeyError:
